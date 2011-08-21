7a59d7dc559763f61204d0c11fb61fa485b096fe
Patrick Lam <prof.lam@gmail.com>
2011-08-21 01:18:39 -0400

    Update version.

--- src/ca/patricklam/judodb/client/ListWidget.java
+++ src/ca/patricklam/judodb/client/ListWidget.java
@@ -1,18 +1,104 @@
 package ca.patricklam.judodb.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ListWidget extends Composite {
 	interface MyUiBinder extends UiBinder<Widget, ListWidget> {}
 	public static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
 	JudoDB jdb;
+	
+	private JsArray<ClientData> allClients;
+	
+	private static final String PULL_ALL_CLIENTS_URL = JudoDB.BASE_URL + "pull_all_clients.php";
+	private static final String CALLBACK_URL_SUFFIX = "?callback=";
+	private int jsonRequestId = 0;
+	
+	@UiField(provided=true) Grid results = new Grid();
+
+	//var heads =     ["", "Nom",   "Prenom", "Sexe", "Grade", "DateGrade", "Tel", "JudoQC", "DDN", "Cat", "V", "Cours", ""];
+	  //var widthsForEditing = [-1, -1, -1, 1, 3, 8, -1, 8, -1, -1, -1, -1, -1];
 
 	public ListWidget(JudoDB jdb) {
 		this.jdb = jdb;
 		initWidget(uiBinder.createAndBindUi(this));
-		
+		getJson(jsonRequestId++, PULL_ALL_CLIENTS_URL + CALLBACK_URL_SUFFIX, this);	
 	}
+
+	public void showList() {
+		ClientData cd;
+		int count = 0, curRow;
+		
+		// two passes: 1) count; 2) populate the grid
+		for (int i = 0; i < allClients.length(); i++) {
+			cd = allClients.get(i);
+			// if (fails filter) continue;
+			count++;
+		}
+		
+		results.resize(13, count);
+		curRow = 0;
+		for (int i = 0; i < allClients.length(); i++) {
+			cd = allClients.get(i);
+			// if (fails filter) continue;
+
+			results.setText(curRow, 1, cd.getNom());
+			results.setText(curRow, 2, cd.getPrenom());
+			curRow++;
+		}
+		Window.alert("count is "+count);
+	}
+	
+	/**
+	 * Make call to remote server.
+	 */
+	public native static void getJson(int requestId, String url,
+	      ListWidget handler) /*-{
+	   var callback = "callback" + requestId;
+
+	   var script = document.createElement("script");
+	   script.setAttribute("src", url+callback);
+	   script.setAttribute("type", "text/javascript");
+	   window[callback] = function(jsonObj) {
+	     handler.@ca.patricklam.judodb.client.ListWidget::handleJsonResponse(Lcom/google/gwt/core/client/JavaScriptObject;)(jsonObj);
+	     window[callback + "done"] = true;
+	   }
+
+	   setTimeout(function() {
+	     if (!window[callback + "done"]) {
+	       handler.@ca.patricklam.judodb.client.ListWidget::handleJsonResponse(Lcom/google/gwt/core/client/JavaScriptObject;)(null);
+	     }
+
+	     document.body.removeChild(script);
+	     delete window[callback];
+	     delete window[callback + "done"];
+	   }, 5000);
+
+	   document.body.appendChild(script);
+	  }-*/;
+
+	/**
+	 * Handle the response to the request for data from a remote server.
+	 */
+	public void handleJsonResponse(JavaScriptObject jso) {
+		if (jso == null) {
+			jdb.displayError("Couldn't retrieve JSON");
+			return;
+		}	
+
+	    this.allClients = asArrayOfClientData (jso);
+	    showList();
+	}
+
+	private final native JsArray<ClientData> asArrayOfClientData(JavaScriptObject jso) /*-{
+		return jso;
+	}-*/;
+
 }
