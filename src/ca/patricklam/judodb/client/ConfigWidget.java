481f3adcf32b9db496ed3230853d89a213620f21
Patrick Lam <prof.lam@gmail.com>
2012-12-04 11:36:13 -0800

    add fields we're going to permit editing

--- src/ca/patricklam/judodb/client/ConfigWidget.java
+++ src/ca/patricklam/judodb/client/ConfigWidget.java
@@ -15,6 +15,46 @@ public class ConfigWidget extends Composite {
 	//private static final String PUSH_CONFIG_URL = JudoDB.BASE_URL + "push_config.php";
 	private static final String CALLBACK_URL_SUFFIX_Q = "?callback=";
 
+	// club data:
+	// nom du club
+	// numero (Judo QC) du club
+	
+	// montants (should theoretically be stored with the session)
+	// passeport judo QC [5]
+	// frais non-anjou [5]
+	// penalty for prorata (percent of cost) [5]
+
+	// misc:
+	// age veteran [35]
+	
+	// for each session:
+	// seqno
+	// abbrev [A12]
+	// effective_year [2013]
+	// debut_cours, fin_cours [newDate("1/Sep/2012"), newDate("11/May/2013"))]
+	// liste des prix:
+	//  division, 1 session, 2 sessions, judoQC
+
+	// for each division:
+	// name [Junior]
+	// abbrev [U20N]
+	// years_ago [20]
+	// noire [true]
+	// aka [U20]
+
+	// for each cours:
+	// 		new Cours("0", "Adultes (LM2015-2145, V2000-2145)", "LM2015 V2000", ""),
+	// seqno [0]
+	// name [Adultes (LM2015-2145, V2000-2145)]
+	// abbrev [LM2015 V2000]
+	// entraineur
+	// which sessions does the cours apply to?
+	
+	// for each escompte:
+	// seqno
+	// name [2e membre]
+	// percent [10]
+	
 	public ConfigWidget(JudoDB jdb) {
 		this.jdb = jdb;
 		initWidget(uiBinder.createAndBindUi(this));
