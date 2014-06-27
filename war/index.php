d4a620644a1da782d161c398c8a93cb86c1295c4
Patrick Lam <prof.lam@gmail.com>
2014-06-26 22:52:08 -0400

    Use OAuth2 (via Google) instead of OpenID.

--- war/index.php
+++ war/index.php
@@ -1,3 +1,60 @@
+<?php
+require 'Client.php';
+require 'GrantType/IGrantType.php';
+require 'GrantType/AuthorizationCode.php';
+require '_config.php';
+
+$GOOGLE_AUTHORIZE_URL="https://accounts.google.com/o/oauth2/auth";
+$GOOGLE_ACCESS_TOKEN_URL="https://accounts.google.com/o/oauth2/token";
+
+if (isset($_GET["error"]))
+{
+    echo("<pre>OAuth Error: " . $_GET["error"]."\n");
+    echo('<a href="index.php">Retry</a></pre>');
+    die;
+}
+
+session_start();
+$client = new OAuth2\Client($GOOGLE_CLIENT_ID, $GOOGLE_CLIENT_SECRET, OAuth2\Client::AUTH_TYPE_FORM);
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] != "yes")
+{
+    if (!isset($_GET["code"]))
+    {
+        $state = md5(rand());
+        $_SESSION['state'] = $state;
+        $authUrl = $client->getAuthenticationUrl($GOOGLE_AUTHORIZE_URL, $LOGIN_URL, array("scope" => "openid email profile", "client_id" => $GOOGLE_CLIENT_EMAIL, "state" => $state));
+        header("Location: ".$authUrl);
+        die("Redirect");
+    }
+    else
+    {
+        if ($_GET['state'] != $_SESSION['state']) {
+            die("state doesn't match");
+        }
+
+        $params = array("code" => $_GET["code"], "redirect_uri" => $LOGIN_URL, "client_id" => $GOOGLE_CLIENT_EMAIL);
+        $response = $client->getAccessToken($GOOGLE_ACCESS_TOKEN_URL, "authorization_code", $params);
+        $accessTokenResult = $response["result"];
+        $client->setAccessToken($accessTokenResult["access_token"]);
+        $client->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_OAUTH);
+
+        $at = $accessTokenResult["access_token"];
+        $response = $client->fetch("https://www.googleapis.com/plus/v1/people/me/openIdConnect")['result'];
+
+        if (isset($response['email_verified']) && $response['email_verified'] == 'true') {
+            $_SESSION['authenticated'] = "yes";
+            $_SESSION['email'] = $response['email'];
+            $_SESSION['plus_identity'] = $response['sub'];
+            header("Location: index.php");
+            die("Redirect");
+        } else {
+            echo 'Usager non r&eacute;connu.';
+            die;
+        }
+    }
+}
+?>
+
 <!doctype html>
 
 <html>
@@ -19,22 +76,9 @@
     <noscript>
       <div style="width: 22em; position: absolute; left: 50%; margin-left: -11em; color: red; background-color: white; border: 1px solid red; padding: 4px; font-family: sans-serif">
         Your web browser must have JavaScript enabled
-        in order for this application to display correctly.
+        for this application to display correctly.
       </div>
     </noscript>
-
-    <?php
-       require_once 'openid.php';
-       require '_config.php';
-       $openid = new LightOpenID($HOST);
-
-       $openid->identity = 'https://www.google.com/accounts/o8/id';
-       $openid->required = array('contact/email');
-       $openid->returnUrl = $LOGIN_URL;
-
-       session_start();
-       if (!isset($_SESSION['authenticated'])) echo '<a href=' . $openid->authUrl(). '>S\'identifier avec Google</a>';
-    ?>
 
    	<p><span id="statusContainer">&nbsp;</span></p>
 
