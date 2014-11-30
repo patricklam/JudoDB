102b1817d1aac823fdb304d89885fc507920c632
Patrick Lam <prof.lam@gmail.com>
2014-11-30 16:06:35 -0500

    remove stub code, now outdated

--- src/ca/patricklam/judodb/client/ConfigWidget.java
+++ src/ca/patricklam/judodb/client/ConfigWidget.java
@@ -62,47 +62,5 @@ public class ConfigWidget extends Composite {
         this.jdb = jdb;
         initWidget(uiBinder.createAndBindUi(this));
         jdb.pleaseWait();
-        getJson(jdb.jsonRequestId++, PULL_CONFIG_URL + CALLBACK_URL_SUFFIX_Q, this);
-    }
-
-    /**
-     * Make call to remote server.
-     */
-    public native static void getJson(int requestId, String url,
-          ConfigWidget handler) /*-{
-       var callback = "callback" + requestId;
-
-       var script = document.createElement("script");
-       script.setAttribute("src", url+callback);
-       script.setAttribute("type", "text/javascript");
-       window[callback] = function(jsonObj) {
-         handler.@ca.patricklam.judodb.client.ListWidget::handleJsonResponse(Lcom/google/gwt/core/client/JavaScriptObject;)(jsonObj);
-         window[callback + "done"] = true;
-       }
-
-       setTimeout(function() {
-         if (!window[callback + "done"]) {
-           handler.@ca.patricklam.judodb.client.ListWidget::handleJsonResponse(Lcom/google/gwt/core/client/JavaScriptObject;)(null);
-         }
-
-         document.body.removeChild(script);
-         delete window[callback];
-         delete window[callback + "done"];
-       }, 5000);
-
-       document.body.appendChild(script);
-      }-*/;
-
-    /**
-     * Handle the response to the request for data from a remote server.
-     */
-    public void handleJsonResponse(JavaScriptObject jso) {
-        if (jso == null) {
-            jdb.displayError("Couldn't retrieve JSON (config)");
-            return;
-        }
-
-        //this.config = jso.cast();
-        jdb.clearStatus();
     }
 }
