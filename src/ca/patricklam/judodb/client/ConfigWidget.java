3197cbeb52158c3d42563e7ea65a82f71c67ae1f
Patrick Lam <prof.lam@gmail.com>
2015-05-19 00:08:04 -0400

    use celltable for config widget for sessions

--- src/ca/patricklam/judodb/client/ConfigWidget.java
+++ src/ca/patricklam/judodb/client/ConfigWidget.java
@@ -9,16 +9,22 @@ import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.ResizeLayoutPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.cell.client.EditTextCell;
+import com.google.gwt.cell.client.FieldUpdater;
+import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.core.client.EntryPoint;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class ConfigWidget extends Composite {
     interface MyUiBinder extends UiBinder<Widget, ConfigWidget> {}
@@ -29,7 +35,8 @@ public class ConfigWidget extends Composite {
     private final JudoDB jdb;
     private static final String PULL_SESSIONS_URL = JudoDB.BASE_URL + "pull_sessions.php";
 
-    @UiField Grid sessions;
+    CellTable sessions;
+    private static final List<SessionSummary> sessionData = new ArrayList<SessionSummary>();
 
     // private static final String PULL_CONFIG_URL = JudoDB.BASE_URL + "pull_config.php";
     // private static final String PUSH_CONFIG_URL = JudoDB.BASE_URL + "push_config.php";
@@ -87,12 +94,176 @@ public class ConfigWidget extends Composite {
       }
     }
 
+    class SessionSummaryFieldUpdaterExample {
+    }
+
+    private static final ProvidesKey<SessionSummary> KEY_PROVIDER =
+	new ProvidesKey<SessionSummary>() {
+        @Override
+        public Object getKey(SessionSummary item) {
+	    return item.getSeqno();
+        }
+    };
+
+    interface ColumnFields<T> {
+	public String getValue(T object);
+	public void setValue(T object, String newValue);
+	public String getName();
+	public int getWidth();
+	public Unit getWidthUnits();
+    }
+
+    private class NameColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getName(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setName(newValue); }
+	@Override public String getName()
+	    { return "Nom"; }
+	@Override public int getWidth()
+	    { return 15; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class AbbrevColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getAbbrev(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setAbbrev(newValue); }
+	@Override public String getName()
+	    { return "Abbr"; }
+	@Override public int getWidth()
+	    { return 4; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class YearColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getYear(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setYear(newValue); }
+	@Override public String getName()
+	    { return "Année"; }
+	@Override public int getWidth()
+	    { return 4; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class SeqnoColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getSeqno(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { }
+	@Override public String getName()
+	    { return "no seq"; }
+	@Override public int getWidth()
+	    { return 3; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class LinkedSeqnoColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getLinkedSeqno(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setLinkedSeqno(newValue); }
+	@Override public String getName()
+	    { return "seq alt"; }
+	@Override public int getWidth()
+	    { return 3; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class FirstClassDateColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getFirstClassDate(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setFirstClassDate(newValue); }
+	@Override public String getName()
+	    { return "debut cours"; }
+	@Override public int getWidth()
+	    { return 5; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class FirstSignupDateColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getFirstSignupDate(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setFirstSignupDate(newValue); }
+	@Override public String getName()
+	    { return "debut inscr"; }
+	@Override public int getWidth()
+	    { return 5; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class LastClassDateColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getLastClassDate(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setLastClassDate(newValue); }
+	@Override public String getName()
+	    { return "fin cours"; }
+	@Override public int getWidth()
+	    { return 5; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+    private class LastSignupDateColumn implements ColumnFields<SessionSummary> {
+	@Override public String getValue(SessionSummary object)
+	    { return object.getLastSignupDate(); }
+	@Override public void setValue(SessionSummary object, String newValue)
+	    { object.setLastSignupDate(newValue); }
+	@Override public String getName()
+	    { return "fin inscr"; }
+	@Override public int getWidth()
+	    { return 5; }
+	@Override public Unit getWidthUnits()
+	    { return Unit.EM; }
+    }
+
+    private void addSessionColumn(final CellTable t, final ColumnFields<SessionSummary> c) {
+	final EditTextCell cell = new EditTextCell();
+	Column<SessionSummary, String> newColumn = new Column<SessionSummary, String>(cell) {
+	    public String getValue(SessionSummary object) {
+		return c.getValue(object);
+	    }
+	};
+	sessions.addColumn(newColumn, c.getName());
+	newColumn.setFieldUpdater(new FieldUpdater<SessionSummary, String>() {
+		@Override
+		public void update(int index, SessionSummary object, String value) {
+		    // XXX push the new name also
+		    c.setValue(object, value);
+		    t.redraw();
+		}
+	    });
+	sessions.setColumnWidth(newColumn, c.getWidth(), c.getWidthUnits());
+    }
+
     public ConfigWidget(JudoDB jdb) {
         this.jdb = jdb;
         initWidget(uiBinder.createAndBindUi(this));
         jdb.pleaseWait();
         jdb.populateClubList(true, dropDownUserClubs);
         dropDownUserClubs.addChangeHandler(clHandler);
+
+	sessions = new CellTable<SessionSummary>(KEY_PROVIDER);
+	sessions.setWidth("60em", true);
+
+	addSessionColumn(sessions, new NameColumn());
+	addSessionColumn(sessions, new AbbrevColumn());
+	addSessionColumn(sessions, new YearColumn());
+	addSessionColumn(sessions, new SeqnoColumn());
+	addSessionColumn(sessions, new LinkedSeqnoColumn());
+	addSessionColumn(sessions, new FirstClassDateColumn());
+	addSessionColumn(sessions, new FirstSignupDateColumn());
+	addSessionColumn(sessions, new LastClassDateColumn());
+	addSessionColumn(sessions, new LastSignupDateColumn());
+
+	sessions.setRowData(sessionData);
+
+	configMain.add(sessions);
     }
 
     private class SessionColumns {
@@ -108,25 +279,12 @@ public class ConfigWidget extends Composite {
     }
 
     private void loadSessions(JsArray<SessionSummary> sessionArray) {
-
-        sessions.resize(sessionArray.length(), 9);
-	// com.google.gwt.user.client.Window.alert(Integer.toString(sessionArray.length()));
+	sessionData.clear();
         for (int i = 0; i < sessionArray.length(); i++) {
-            SessionSummary s = sessionArray.get(i);
-
-            sessions.setText(i, SessionColumns.SEQNO, s.getSeqno());
-            sessions.setText(i, SessionColumns.NAME, s.getName());
-            sessions.setText(i, SessionColumns.ABBREV, s.getAbbrev());
-            sessions.setText(i, SessionColumns.YEAR, s.getYear());
-            sessions.setText(i, SessionColumns.LINKED_SEQNO, s.getLinkedSeqno());
-            sessions.setText(i, SessionColumns.FIRST_CLASS_DATE, s.getFirstClassDate());
-            sessions.setText(i, SessionColumns.FIRST_SIGNUP_DATE, s.getFirstSignupDate());
-            sessions.setText(i, SessionColumns.LAST_CLASS_DATE, s.getLastClassDate());
-            sessions.setText(i, SessionColumns.LAST_SIGNUP_DATE, s.getLastSignupDate());
-
-            //cours.addItem(c.getShortDesc(), c.getId());
-            //backingCours.add(c);
-        }
+	    sessionData.add(sessionArray.get(i));
+	}
+	sessions.setRowData(sessionData);
+	sessions.redraw();
     }
 
     /* --- network functions --- */
