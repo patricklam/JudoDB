5fb71312d2df00c4e5081f383e4221b107cf1371
Patrick Lam <prof.lam@gmail.com>
2015-05-21 20:46:56 -0400

    implement push of session_club info as well (modifying existing)

--- src/ca/patricklam/judodb/client/SessionSummary.java
+++ src/ca/patricklam/judodb/client/SessionSummary.java
@@ -34,10 +34,10 @@ public class SessionSummary extends JavaScriptObject {
 	if (key.equals("abbrev")) return getAbbrev();
 	if (key.equals("id")) return getId();
 	if (key.equals("club")) return getClub();
-	if (key.equals("firstClassDate")) return getFirstClassDate();
-	if (key.equals("firstSignupDate")) return getFirstSignupDate();
-	if (key.equals("lastClassDate")) return getLastClassDate();
-	if (key.equals("lastSignupDate")) return getLastSignupDate();
+	if (key.equals("first_class_date")) return getFirstClassDate();
+	if (key.equals("first_signup_date")) return getFirstSignupDate();
+	if (key.equals("last_class_date")) return getLastClassDate();
+	if (key.equals("last_signup_date")) return getLastSignupDate();
 	throw new RuntimeException("get: bad key " + key);
     }
 
@@ -50,10 +50,10 @@ public class SessionSummary extends JavaScriptObject {
 	else if (key.equals("year")) setYear(value);
 	else if (key.equals("abbrev")) setAbbrev(value);
 	else if (key.equals("club")) setClub(value);
-	else if (key.equals("firstClassDate")) setFirstClassDate(value);
-	else if (key.equals("firstSignupDate")) setFirstSignupDate(value);
-	else if (key.equals("lastClassDate")) setLastClassDate(value);
-	else if (key.equals("lastSignupDate")) setLastSignupDate(value);
+	else if (key.equals("first_class_date")) setFirstClassDate(value);
+	else if (key.equals("first_signup_date")) setFirstSignupDate(value);
+	else if (key.equals("last_class_date")) setLastClassDate(value);
+	else if (key.equals("last_signup_date")) setLastSignupDate(value);
 	else throw new RuntimeException("set: bad key " + key);
     }
 }
